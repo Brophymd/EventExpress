1070ceb045ab8ff7a449407b5a9032b1c6169239
varik31 <mark.d.brophy@gmail.com>
2014-10-11 16:21:38 -0400

    First
    Firist

---
+++ src/edu/usf/EventExpress/Login.java
@@ -0,0 +1,16 @@
+package edu.usf.EventExpress;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Varik on 10/11/2014.
+ */
+public class Login extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    // a change
+    }
+}
