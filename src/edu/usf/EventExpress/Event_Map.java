948e913e80c14a0d3662aaaee9376dc1b6a7e8c8
varik31 <mark.d.brophy@gmail.com>
2014-10-27 16:12:04 -0400

    Map now finds the user's location and zooms in on it.

--- src/edu/usf/EventExpress/Event_Map.java
+++ src/edu/usf/EventExpress/Event_Map.java
@@ -2,10 +2,16 @@ package edu.usf.EventExpress;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by Varik on 10/26/2014.
@@ -13,28 +19,34 @@ import com.google.android.gms.maps.MapFragment;
 public class Event_Map extends Activity {
 
     private GoogleMap googleMap;
+    private LocationManager locationManager;
+    private Location myLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_map);
 
-
-        //InitilizeMap();
+        InitilizeMap();
 
     }
 
-    /*private void InitilizeMap() {
+    private void InitilizeMap() {
+        LatLng mylatlng;
         if (googleMap == null) {
-            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-                    R.id.map)).getMap();
-
-            // check if map is created successfully or not
-            if (googleMap == null) {
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                        .show();
-            }
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         }
-    }*/
+        if (locationManager == null){
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        }
+        if (myLocation == null){
+            myLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider( new Criteria(), true));
+        }
+        googleMap.setMyLocationEnabled(true);
+        mylatlng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(mylatlng));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+
+
+    }
 }
