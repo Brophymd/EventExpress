21191f0f1acb81c0755ff6f80d8f773bae92cf84
rums <mercyfulfate@gmail.com>
2014-11-17 00:54:21 -0500

    turned on extra retrofit logs

--- src/edu/usf/EventExpress/sync/SyncHelper.java
+++ src/edu/usf/EventExpress/sync/SyncHelper.java
@@ -35,6 +35,7 @@ public class SyncHelper {
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
                 .create();
         RestAdapter restAdapter = new RestAdapter.Builder()
+                .setLogLevel(RestAdapter.LogLevel.FULL)
                 .setEndpoint(EventServer.API_URL)
                 .setConverter(new GsonConverter(gson))
                 .build();
